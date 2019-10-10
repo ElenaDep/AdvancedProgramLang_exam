934bff435fb7923bb2a0ef0aca42e50cf5473a77
Elena De Paoli <elena.dpaoli@gmail.com>
2019-10-10 20:47:13 +0200

    reverse order exercise

--- exercises/c++/02_arrays/reverse.cc
+++ exercises/c++/02_arrays/reverse.cc
@@ -4,6 +4,9 @@
 template <typename T>
 T* init(std::size_t n, T* a);
 
+//template <typename T>	prova per fare un array di tutti i tipi
+//T* initiauto(std::size_t n);
+
 template <typename T>
 void print_a(const T* a, std::size_t n);
 
@@ -12,12 +15,13 @@ int main () {
   std::cout << "Please insert array lenght: ";
   std::size_t n;
   std::cin >> n;
-  
-  int* const a{init<int>(n, a)};
+   
+  int* const a{init<int>(n,a)};
+  //auto* const a{init(n)};
 
   for(std::size_t i{0}; i<n; i++){
     std::cin>>*(a+i);
-  }
+   }
 
   print_a<int>(a,n);  
 
@@ -26,8 +30,13 @@ return 0;
 }
 
 template <typename T>
-T* init(std::size_t n, T* a){
-  return new T[n];  // allocated on the HEAP 
+T* init(std::size_t n, T* a){			//con auto* const a sarebbe T* solo init(std::size_t n)
+   return new T[n];  // allocated on the HEAP 
+   
+   //auto * const a=new T[n];  prova per fare un array di tutti i tipi, ma non funziona
+   //for(std::size_t i{0}; i<n; i++){std::cin>>*(a+i);}
+   //return a;
+
 }
 
 template <typename T>
