a01d5e917849118a495f7d23ecb33df1dad9e0df
Elena De Paoli <elena.dpaoli@gmail.com>
2019-10-11 09:44:48 +0200

    auto reverse order

--- exercises/c++/02_arrays/reverse.cc
+++ exercises/c++/02_arrays/reverse.cc
@@ -2,7 +2,7 @@
 #include<cstring>
 
 template <typename T>
-T* init(std::size_t n, T* a);
+T* init(std::size_t n);
 
 //template <typename T>	prova per fare un array di tutti i tipi
 //T* initiauto(std::size_t n);
@@ -16,8 +16,10 @@ int main () {
   std::size_t n;
   std::cin >> n;
    
-  int* const a{init<int>(n,a)};
-  //auto* const a{init(n)};
+  //int* const a{init<int>(n)};
+
+  auto a = init<int>(n);
+  //auto b = init<double>(n);
 
   for(std::size_t i{0}; i<n; i++){
     std::cin>>*(a+i);
@@ -30,7 +32,7 @@ return 0;
 }
 
 template <typename T>
-T* init(std::size_t n, T* a){			//con auto* const a sarebbe T* solo init(std::size_t n)
+T* init(std::size_t n){			//con auto* const a sarebbe T* solo init(std::size_t n)
    return new T[n];  // allocated on the HEAP 
    
    //auto * const a=new T[n];  prova per fare un array di tutti i tipi, ma non funziona
